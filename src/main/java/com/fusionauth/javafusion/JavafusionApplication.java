9708c420c7a7f719d63bdb1f2448e76d2cbe9a9e
Bradley Van Aardt <bradley@indri.co.za>
2022-10-25 17:26:35 +0200

    basic example

---
+++ src/main/java/com/fusionauth/javafusion/JavafusionApplication.java
@@ -0,0 +1,13 @@
+package com.fusionauth.javafusion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavafusionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavafusionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fusionauth/javafusion/JavafusionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fusionauth.javafusion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavafusionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
